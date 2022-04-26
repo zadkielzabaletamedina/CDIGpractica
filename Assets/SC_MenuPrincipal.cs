6bc94668a099c046309aa75a5531de119f362eda
bq0306 <zadkiel.zabaleta.medina@alumno.upm.es>
2022-04-26 12:25:59 +0200

    Subir

---
+++ Assets/SC_MenuPrincipal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_MenuPrincipal : MonoBehaviour {
+
+	GameObject MenuPrincipal;
+	GameObject Creditos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void CreditsButtom (){
+		MenuPrincipal.SetActive(false);
+		Creditos.SetActive(true);
+	}
+
+	void QuitButtom() {
+		Application.Quit();
+	}
+}
